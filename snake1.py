dd8583f274670ec91e5d041296e9b24277d9a3b7
DariusBlaga18 <blagagabi@gmail.com>
2025-11-24 13:04:22 +0200

    Initial commit - Snake Game 1

---
+++ snake1.py
@@ -0,0 +1,45 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Snake Game 1")
+screen.bgcolor("black")
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+head = turtle.Turtle()
+head.shape("square")
+head.color("red")
+head.penup()
+head.goto(0, 0)
+head.speed(0)
+
+head.direction = "stop"
+
+def go_up():
+    head.direction = "up"
+def go_down():
+    head.direction = "down"
+def go_right():
+    head.direction = "right"
+def go_left():
+    head.direction = "left"
+
+screen.listen()
+screen.onkeypress(go_up, "Up")
+screen.onkeypress(go_down, "Down")
+screen.onkeypress(go_left, "Left")
+screen.onkeypress(go_right, "Right")
+
+def move():
+    if head.direction == "up":
+        head.sety( head.ycor() + 20)
+    if head.direction == "down":
+        head.sety( head.ycor() - 20)
+    if head.direction == "right":
+        head.setx( head.xcor() + 20)
+    if head.direction == "left":
+        head.setx( head.xcor() - 20)
+
+while True:
+    screen.update()
+    move()
